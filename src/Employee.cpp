8e7c42a9ec106881356cf3a135b1fc9c0fb207ab
hoangnnh <huyhoang.ngoc.nguyen@gmail.com>
2024-03-26 23:00:09 +0700

    Add src directory

---
+++ src/Employee.cpp
@@ -0,0 +1,18 @@
+/*
+ * Employee.cpp
+ *
+ *  Created on: Mar 26, 2024
+ *      Author: hoangnnh
+ */
+
+#include "Employee.h"
+
+Employee::Employee() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Employee::~Employee() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Employee.h
@@ -0,0 +1,17 @@
+/*
+ * Employee.h
+ *
+ *  Created on: Mar 26, 2024
+ *      Author: hoangnnh
+ */
+
+#ifndef SRC_EMPLOYEE_H_
+#define SRC_EMPLOYEE_H_
+
+class Employee {
+public:
+	Employee();
+	virtual ~Employee();
+};
+
+#endif /* SRC_EMPLOYEE_H_ */
